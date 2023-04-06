ad08e42b91102d638c27dfc7592d565aeb29e410
Justus Hübotter <justus.huebotter@donders.ru.nl>
2023-04-06 23:48:40 +0200

    initial fix

--- control_stork/nodes/lif/base.py
+++ control_stork/nodes/lif/base.py
@@ -59,7 +59,7 @@ class LIFGroup(CellGroup):
         )
         self.tau_mem = tau_mem
         self.tau_syn = tau_syn
-        self.spk_nl = activation.apply
+        self.spk_nl = activation
         self.diff_reset = diff_reset
         self.learn_timescales = learn_timescales
         self.clamp_mem = clamp_mem
